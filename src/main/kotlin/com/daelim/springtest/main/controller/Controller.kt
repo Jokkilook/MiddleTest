e3dea721ce461ac4cbe39936ef60c19de71acf76
Jokkilook <tjddbs8295@gmail.com>
2024-04-24 15:33:48 +0900

    로또번호생성

--- src/main/kotlin/com/daelim/springtest/main/controller/Controller.kt
+++ src/main/kotlin/com/daelim/springtest/main/controller/Controller.kt
@@ -16,6 +16,28 @@ class Controller {
     private val userList = mutableListOf<UserDto>()
     private val lottoNumList = mutableListOf<LottoDto>()
 
+    @GetMapping("/lotto")
+    fun getLotto(): ResponseEntity<LottoDto> {
+        var resultList = mutableListOf<Array<Int>>()
+
+        for(n:Int in 1..5) {
+            val numList = mutableListOf<Int>()
+            for (i: Int in 1..45) {
+                numList.add(i)
+            }
+            numList.shuffle()
+            var array = arrayOf(numList[0], numList[1], numList[2], numList[3], numList[4], numList[5], numList[6])
+            array.sort()
+            resultList.add(array)
+        }
+
+
+        var response = LottoDto(numbers = resultList)
+
+
+        return ResponseEntity.ok().body(response)
+    }
+
 }
 
 
